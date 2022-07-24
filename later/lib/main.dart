2db57cd37eaf3bda1b0e3eea1abb15c80cc65796
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-07-24 16:33:59 +0300

    start implementing the app Later

---
+++ later/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+void main() {
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/langs',
+      fallbackLocale: const Locale('en'),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: Home(),
+    );
+  }
+}
