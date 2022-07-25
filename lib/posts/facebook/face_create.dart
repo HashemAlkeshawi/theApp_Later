e8a1301753315b101eb3d05fa0108eb03d576ec3
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-07-25 17:32:25 +0300

    fealing

--- lib/posts/facebook/face_create.dart
+++ lib/posts/facebook/face_create.dart
@@ -1,15 +1,61 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FaceCreate extends StatelessWidget {
   static const String screenName = "FaceCreate";
+
+  TextEditingController contentController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text("NFPost".tr()),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.done,
+                size: 35.r,
+              ))
+        ],
       ),
-      body: Container(),
+      body: Container(
+          padding: EdgeInsets.all(12.h),
+          height: screenHeight,
+          child: ListView(
+            children: [
+              TextField(
+                controller: contentController,
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "FCH".tr(),
+                ),
+              ),
+              Container(
+                child: null,
+              ),
+            ],
+          )),
+      bottomNavigationBar: SizedBox(
+          height: 200.h,
+          child: Column(
+            children: [
+              const Divider(),
+              ListTile(
+                leading: Image.asset("assets/images/add_photo.png"),
+                title: Text("Photo".tr()),
+              ),
+              const Divider(),
+              DropdownButton(
+                
+                leading: Image.asset("assets/images/feeling.png"),
+                title: Text("Feeling".tr()),
+              )
+            ],
+          )),
     );
   }
 }
